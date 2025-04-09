7c650e48967af57d974a300ebc36dd47e11cb529
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-04-09 21:18:24 +0900

    api通信OK

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuestionForm from './QuestionForm'; // QuestionFormコンポーネントをインポート
+
+function App() {
+  return (
+    <div>
+      <QuestionForm /> {/* QuestionFormコンポーネントをここで使用 */}
+    </div>
+  );
+}
+
+export default App;
