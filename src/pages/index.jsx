c9bbdb5797b6d1f0f11fcd6703985f72b43f499f
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-04-10 14:01:18 +0900

    vercleへのデプロイバグ修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,17 @@
 // pages/index.jsx
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import ScoreButtons from '../components/select/select'; // ScoreButtonsコンポーネントをインポート
+import ScoreButtons from '../components/Select/ScoreButtons'; // もしファイル名がScoreButtons.jsxの場合
+
 
 const Home = () => {
   return (
     <div>
-
+      <Header /> {/* ヘッダーコンポーネントを追加 */}
+      
       <ScoreButtons /> {/* ScoreButtonsコンポーネントをここで使用 */}
- 
+      
+      <Footer /> {/* フッターコンポーネントを追加 */}
     </div>
   );
 };
